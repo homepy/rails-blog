cb77fefe51bd498111279a13065d8ad2c508ebf2
hopex <hopex40@163.com>
2013-06-16 02:58:20 +0800

    gettinng started

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ rails-blog
 ==========
 
 A rails project called blog in "Ruby on Rails Guides"
+http://guides.rubyonrails.org/getting_started.html
+
+ruby 1.9.3p392
+Rails 3.2.13
---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '9a7fc3faafef6aa4a1f4d3a50edc136df50e75abf6955f749c6186d88b7029cac8ae94443ae7ddc36ed1731ef4d82cf49b94adf29bb27c6b6442cc911de319e8'
